2164c1c8596ecf64ac506faacbbadcb408bb7dfe
Raimundo Ramos <rramos@tractian.com>
2024-07-19 15:57:54 -0300

    initial project configuration

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_root_returns_ok_and_hello_world():
+    client = TestClient(app)
+    response = client.get("/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "Hello, World!"}
